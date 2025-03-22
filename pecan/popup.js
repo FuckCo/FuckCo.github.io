4f2119206b95c84e6ae2d57e01d1fe8305ec2400
MC28-Bruv <mc28dude@gmail.com>
2025-03-22 04:15:17 -0400

    Create popup.js

---
+++ pecan/popup.js
@@ -0,0 +1,4 @@
+document.getElementById('startButton').addEventListener('click', function() {
+  console.log('PECAN Ad Attracter has been activated!');
+  alert('PECAN is now attracting ads!');
+});
